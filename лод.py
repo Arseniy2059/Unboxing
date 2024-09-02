a3c5545005a37782844b1ceee208515065d602c2
Arseniy2059 <semyashulkovy64@mail.ru>
2024-09-02 13:28:34 +0300

    Add files via upload

---
+++ лод.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words, same_words=[]):
+    print(root_word, *other_words, same_words)
+    same_words = "bee", "bumblebee", "сetoniinae", "Darwin's spider", "dragonfly"
+
+
+    for i in same_words:
+        Append,List
+
+
+
+
+
