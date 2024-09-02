04319386e8dc00876052ca2ac8a9a09a900e4e5c
Arseniy2059 <semyashulkovy64@mail.ru>
2024-09-02 13:30:30 +0300

    Update лод.py

--- лод.py
+++ лод.py
@@ -1,6 +1,6 @@
 def single_root_words(root_word, *other_words, same_words=[]):
     print(root_word, *other_words, same_words)
-    same_words = "bee", "bumblebee", "сetoniinae", "Darwin's spider", "dragonfly"
+    same_words = "bee", "bumblebee", "wasp", "Darwin's spider", "dragonfly"
 
 
     for i in same_words:
